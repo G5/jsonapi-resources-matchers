e56fd3938be2c279e1fee7acb755645dad227577
Ramon Tayag <ramon.tayag@gmail.com>
2015-07-20 11:33:48 +0800

    Add a Rails dummy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require File.expand_path("../dummy/config/environment", __FILE__)
 require 'pry'
-require 'rails/all'
 require 'jsonapi-resources'
 require 'jsonapi-resources-matchers'
